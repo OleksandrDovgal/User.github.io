0d162556d67dafbb9de38eb90a1b32acd15854ac
Oleksandr <olexander.dovgal@gmail.com>
2021-02-16 19:39:48 +0200

    added cart counter

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-let productscountEl = document.getElementById("products-count");
-console.log(productscountEl);
+// add to cart
+let productsCountEl = document.getElementById("products-count");
+let addToCartButtons = document.querySelectorAll(".add_to_cart");
 
-let addToCartButtons = document.getElementsByClassName("btn two");
 console.log(addToCartButtons);
+
+for (let i = 0; i < addToCartButtons.length; i++) {
+	addToCartButtons[i].addEventListener("click",function() {
+		// let prevProductsCount = +productsCountEl.textContent;
+		// productsCountEl.textContent = prevProductsCount+1;
+		productsCountEl.textContent = +productsCountEl.textContent + 1;
+		//скорочений запис верхніх 2х рядків коду
+	})
+}
+
+// let addToCartButtons = document.querySelectorAll(".btn two");
+// console.log(addToCartButtons);
