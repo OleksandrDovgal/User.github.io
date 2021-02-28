15a2d8123a546dcfaa82ae1010110e91599e7652
Oleksandr <olexander.dovgal@gmail.com>
2021-02-28 09:55:28 +0200

    added slider slick jQuery, added modal window, added quantity

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let productsCountEl = document.getElementById("products-count");
 let addToCartButtons = document.querySelectorAll(".add_to_cart");
 console.log(addToCartButtons);
 
-for (let i = 0; i < addToCartButtons.length; i++) {
+for (let i=0; i<addToCartButtons.length; i++) {
 	addToCartButtons[i].addEventListener("click",function() {
 		// let prevProductsCount = +productsCountEl.textContent;
 		// productsCountEl.textContent = prevProductsCount+1;
@@ -12,29 +12,108 @@ for (let i = 0; i < addToCartButtons.length; i++) {
 	})
 }
 
+let likeBtns = document.querySelectorAll(".like");
+console.log(likeBtns);
+
+for( let i=0; i<likeBtns.length; i++) {
+	likeBtns[i].addEventListener("click",function() {
+	this.classList.toggle("liked")
+}) 
+}
+
+let moreDetailsBtns = document.querySelectorAll(".one");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
 
 
+moreDetailsBtns.forEach(btn => (
+	btn.addEventListener("click",function()  {
+		modal.classList.add("show");
+		modal.classList.remove("hide");
+	})
+))
 
-let favorite = document.getElementById("favorite");
-console.log(favorite);
-favorite.addEventListener("click", function(e) {
-	e.target.style.backgroundColor = "black"
+closeBtn.addEventListener("click",function() {
+	modal.classList.add("hide");
+	modal.classList.remove("show");
 })
 
-// favorite.addEventListener("click", function(e) {
-// 	favorite.style.backgroundColor = "grey"
-// })
+
+closeBtn.addEventListener("click",closeModal) 
+
+function openModal() {
+	modal.classList.add("hide");
+	modal.classList.remove("show");
+}
+
+function closeModal() {
+	modal.classList.add("hide");
+	modal.classList.remove("show");
+}
+
+modal.addEventListener("click",function(e) {
+	console.log(e.target);
+	if(e.target===modal){
+		closeModal();
+	}
+})
+
+ 
+function showModalByScroll() {
+	if(window.pageYOffset>document.body.scrollHeight/2) {
+		 openModal();
+		 window.removeEventListener("scroll",showModalByScroll);
+	}
+}
+
+window.addEventListener("scroll",showModalByScroll);
 
 
-// let favorite = document.getElementById("favorite");
 
-// favorite.addEventListener("click", function(e) {
-// 	e.target.style.backgroundColor = "red";
-// })
 
-// let like = document.querySelectorAll(".for_js");
-// console.log(like);
 
-// like.addEventListener("click", function(e) {
-// 	e.targt.style.backgroundColor = "red";
-// })
+// change product quantity
+let decrementBtns = document.querySelectorAll(".decrement-button");
+let incrementBtns = document.querySelectorAll(".increment-button");
+let productsQuantity = document.querySelectorAll(".product-quantity input");
+
+for( let i = 0; i<productsQuantity.length; i++) {
+	let currentCount = +productsQuantity[i].value;
+
+	toggleButtonState(currentCount, decrementBtns[i],incrementBtns[i])
+}
+
+function toggleButtonState(count, decrementBtns, incrementBtns) {
+	decrementBtns.disabled = count <= 1;
+	incrementBtns.disabled = count >= 10;
+}
+
+for(let i =0; i < incrementBtns.length; i++) {
+	incrementBtns[i].addEventListener("click",function() {
+		let currentCount = +productsQuantity[i].value;
+		let nextCount = currentCount + 1;
+		productsQuantity[i].value = nextCount;
+	
+		toggleButtonState(nextCount, decrementBtns[i], incrementBtns[i]);
+	})
+}
+
+
+for( let i =0; i < decrementBtns.length; i++) {
+	decrementBtns[i].addEventListener("click",function() {
+		let currentCount = +productsQuantity[i].value;
+		let nextCount = currentCount - 1;
+		productsQuantity[i].value = nextCount;
+	
+		toggleButtonState(nextCount, decrementBtns[i], incrementBtns[i]);
+	})
+}
+
+
+/// add slider slick
+
+$(".slider-block").slick({
+
+
+	
+});
